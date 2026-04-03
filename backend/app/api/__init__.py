405c2292f19228fc06e3460a641da0446f4aa97b
InternalShadow <76181682+InternalShadow@users.noreply.github.com>
2026-04-03 13:02:38 -0400

    api design for blueprint creation from pdf

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from app.api.blueprints import router as blueprints_router
 from app.api.templates import router as templates_router
 
 api_router = APIRouter()
 api_router.include_router(templates_router)
+api_router.include_router(blueprints_router)
 
 __all__ = ["api_router"]
